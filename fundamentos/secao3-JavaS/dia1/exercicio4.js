5c75358c4b92aac76cdfa13fa1dcb885f40c3338
Joao Vitor <jvitorss2021@gmail.com>
2023-07-05 22:36:16 -0300

    ex4

---
+++ fundamentos/secao3-JavaS/dia1/exercicio4.js
@@ -0,0 +1,15 @@
+let ang1 = 40;
+let ang2 = 20;
+let ang3 = 100;
+
+let somadang = ang1 + ang2 + ang3;
+
+if (ang1 > 0 && ang2 > 0 && ang3 > 0){
+    if (somadang === 180){
+        console.log(true);
+}     else{
+        console.log(false);
+        }
+}else {
+    console.log('error');
+}
