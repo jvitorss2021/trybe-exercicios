1728d21fd772401e35bcec3265974ba1fbac091c
Joao Vitor <jvitorss2021@gmail.com>
2023-07-05 22:09:30 -0300

    ex 3

---
+++ fundamentos/secao3-JavaS/dia1/exercicio3.js
@@ -0,0 +1,13 @@
+let num1 = 6
+let num2 = 10
+let num3 = 10
+
+if (num1 > num2 && num1> num3){
+    console.log(num1);
+}else if (num2 > num1 && num2 > num3){
+    console.log(num2);
+}else if (num3 > num1 && num3 > num2){
+    console.log(num3);
+}else {
+    console.log('nao tem numero maior');
+}
