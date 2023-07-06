e1485653688eb96373e6e0ecac8bc9b1ddb16984
Joao Vitor <jvitorss2021@gmail.com>
2023-07-05 21:38:01 -0300

    dia1js

---
+++ fundamentos/secao3-JavaS/dia1/exercicio1.js
@@ -0,0 +1,13 @@
+let num1 = 4;
+let num2 = 5;
+
+soma = num1 + num2
+console.log(soma);
+sub = num1 - num2;
+console.log(sub);
+mult = num1 * num2;
+console.log(mult);
+div = num1 * num2;
+console.log(div);
+mod = num1 % num2; 
+console.log(mod);
