df1142a9210b408848eee56c7b14356d18c6bc7f
Joao Vitor <jvitorss2021@gmail.com>
2023-07-05 23:15:47 -0300

    ex6

---
+++ fundamentos/secao3-JavaS/dia1/exercicio6.js
@@ -0,0 +1,13 @@
+let num1 = 5
+let num2 = 5
+let num3 = 6
+
+let Sobra = false;
+
+if ((num1 % 2 == 0 || num2 % 2 == 0 || num3 %2 == 0)){
+    Sobra = true;
+    // console.log(true);
+// }else {
+    // console.log(false);
+};
+console.log(Sobra);
